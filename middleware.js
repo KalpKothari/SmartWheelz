83ec2f3691e8aa55ac544d4b24a38880c7067b9b
KalpKothari <kalpkothari14@gmail.com>
2026-04-14 13:36:06 +0530

    Refactor middleware for route protection and matcher
    Refactor middleware to improve route protection and optimize matcher.

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
-import { clerkMiddleware, createRouteMatcher,  } from "@clerk/nextjs/server";
-import arcjet, {detectBot, shield, createMiddleware } from "@arcjet/next";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import arcjet, { detectBot, shield, createMiddleware } from "@arcjet/next";
 import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
@@ -11,26 +11,28 @@ const isProtectedRoute = createRouteMatcher([
 const aj = arcjet({
     key: process.env.ARCJET_KEY,
     rules: [
-        // Shield Protection for content and security
-        shield({
-            mode:"LIVE",
-        }),
+        shield({ mode: "LIVE" }),
         detectBot({
-            mode:"LIVE",
-            allow: ["CATEGORY:SEARCH_ENGINE"], //Google,Bing,etc
+            mode: "LIVE",
+            allow: ["CATEGORY:SEARCH_ENGINE"],
         }),
     ],
 });
 
-const clerk =  clerkMiddleware(async (auth,req)=>{
-    const {userId} = await auth();
-
-    if (!userId && isProtectedRoute(req)) {
-        const {redirectToSignIn} = await auth();
-        return redirectToSignIn();
+const clerk = clerkMiddleware(async (auth, req) => {
+    if (isProtectedRoute(req)) {
+        await auth.protect();
     }
-
     return NextResponse.next();
 });
- export default createMiddleware(aj, clerk);
-export const config = { matcher: ["/(.*)"] };
+
+export default createMiddleware(aj, clerk);
+
+export const config = {
+  matcher: [
+    // This optimized matcher excludes static files (images, css, etc.) 
+    // which prevents the middleware from running on things it doesn't need to protect.
+    '/((?!_next|[^?]*\\.(?:html|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
+};
